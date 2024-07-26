a9b5eb897f6d57793c4d46844b8a652f4970e915
Matheus Queiroz <87786441+madmattp@users.noreply.github.com>
2024-07-25 21:06:24 -0300

    Develop speech_to_text function

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import speech_recognition as sr
+from pydub import AudioSegment
 
-def speech_to_text():
-    pass
+def speech_to_text(audio_file, language):
+    #Ex: trasncript = speech_to_text(audio_file="file.mp3", language="pt-BR")
 
-if __name__ == "__main__":
-    #main()
+    audio = AudioSegment.from_file(audio_file)
+    audio.export("audio_temp.wav", format="wav")
+
+    recognizer = sr.Recognizer()
+    with sr.AudioFile("audio_temp.wav") as source:
+        audio_data = recognizer.record(source)
+
+        try:
+            text = recognizer.recognize_google(audio_data, language=language)
+            return text
+        except Exception as e:
+            print(e)
+            return None
